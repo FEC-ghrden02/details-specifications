c1f240f9fc35013b69067912035aa4c9f287f789
Martin Naparstek <tinchonapa@gmail.com>
2020-01-27 16:43:26 -0700

    Established client side

---
+++ client/src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class App extends React.Component {
+    constructor(){
+        super();
+    }
+
+    render() {
+        return (
+        <div>
+            <h3>Detailed Specifications</h3>
+        </div>
+        )}
+}
+
+export default App;
---
+++ client/src/index.jsx
@@ -0,0 +1,9 @@
+// import React Library
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// import App component
+import App from './components/App.jsx';
+// render App component into the DOM
+
+ReactDOM.render(<App />, document.getElementById('app'));
