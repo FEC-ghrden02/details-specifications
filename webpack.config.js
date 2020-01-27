9721f0770d25b5df28ec65854900a7357a64d789
Martin Naparstek <tinchonapa@gmail.com>
2020-01-27 16:41:32 -0700

    updated structure

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+const webpack = require('webpack');
+var SRC_DIR = path.join(__dirname, '/client/src');
+var DIST_DIR = path.join(__dirname, '/client/dist');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'main.js',
+    path: DIST_DIR
+  },
+  module : {
+    loaders : [
+      {
+        test : /\.jsx?/,
+        include : SRC_DIR,
+        loader : 'babel-loader',      
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  }
+};
