220e732925399674251da344d2c232be7ee84b21
Martin Naparstek <tinchonapa@gmail.com>
2020-01-27 23:06:56 -0700

    Added init structure of React components

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -7,8 +7,10 @@ class App extends React.Component {
 
     render() {
         return (
-        <div>
-            <h3>Detailed Specifications</h3>
+        <div className="ddc-content inventory-detail-tech-specs">
+            <h3 className="h1 ddc-heading-h1 widget-heading">
+                <span>Detailed Specifications</span>
+            </h3>
         </div>
         )}
 }
