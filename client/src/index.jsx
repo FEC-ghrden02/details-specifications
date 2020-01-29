116137e6e0f0069fd9065294a13b0096527dc2ae
Martin Naparstek <tinchonapa@gmail.com>
2020-01-29 12:00:15 -0700

    Cleaned repository and added comments

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,6 +4,6 @@ import ReactDOM from 'react-dom';
 
 // import App component
 import App from './components/App.jsx';
-// render App component into the DOM
 
+// render App component into the DOM
 ReactDOM.render(<App />, document.getElementById('app'));
--- server/index.js
+++ server/index.js
@@ -3,9 +3,22 @@ const express = require ('express');
 const app = express();
 // const db = require('./db/orm/index'); // ORM DB
 const db = require('./db/mongo/index'); // NON-ORM DB
+const Vehicle = require('./db/mongo/vehicle');
 
-const PORT = process.env.PORT || 3000;
-
+// directory for static files
 app.use(express.static(__dirname + '/../client/dist'));
 
+// route for GET /vehicles
+app.get('/api/vehicles', function (req, res){
+    // query to retrieve all documents in the vehicle collection
+    Vehicle.find({}, function(err, docs){
+        res.json(docs);
+    });
+});
+
+app.get('/detailed-specs', function (req, res){
+})
+
+// connection to localhost
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`server running on port: ${PORT}`))
