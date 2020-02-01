6920a8a169582f47d52ec0a4b5c05a10bc31c253
Martin Naparstek <tinchonapa@gmail.com>
2020-01-31 19:38:42 -0700

    Added route communication with client React

--- server/index.js
+++ server/index.js
@@ -5,25 +5,53 @@ const app = express();
 const db = require('./db/mongo/index'); // NON-ORM DB
 const Vehicle = require('./db/mongo/vehicle');
 const ConvenienceFeature = require('./db/mongo/convenience');
+const EntmtFeat = require('./db/mongo/entretainment');
+const OffRoadCapability = require('./db/mongo/offRoad');
+const SeatTrim = require('./db/mongo/seatTrim');
+const SpecsDimen = require('./db/mongo/specsDimen');
 
 // directory for static files
 app.use(express.static(__dirname + '/../client/dist'));
 
 // route for GET /vehicles
-app.get('/api/vehicles', function (req, res){
+app.get ( '/api/vehicles', function (req, res) {
     // query to retrieve all documents in the vehicle collection
-    Vehicle.find({}, function(err, docs){
+    Vehicle.find( {}, function (err, docs){
         res.json(docs);
     });
 });
 
-app.get('/api/convenience-specs', function (req, res){
+app.get ( '/api/convenience-specs', function (req, res) {
     console.log('Request recieved from React')
-    // fix query to remove empty fields
-    ConvenienceFeature.find({}, function (err, docs){
+    // FIX: query to remove empty fields
+    ConvenienceFeature.find( {}, function (err, docs) {
         res.json(docs);
     });
 });
+
+app.get ( '/api/entretainment-specs', function (req, res) {
+    EntmtFeat.find( {}, function (err, docs) {
+        res.json(docs);
+    })
+});
+
+app.get ( '/api/off-road-specs', function (req, res) {
+    OffRoadCapability.find( {}, function (err, docs) {
+        res.json(docs);
+    })
+});
+
+app.get( '/api/seat-trim-specs', function (req, res) {
+    SeatTrim.find( {}, function (err, docs) {
+        res.json(docs);
+    })
+});
+
+app.get ( '/api/specs-dimensions', function (req, res) {
+    SpecsDimen.find( {}, function (err, docs) {
+        res.json(docs);
+    })
+})
 
 // connection to localhost
 const PORT = process.env.PORT || 3000;
